83b96143b21a10b7143851482553a33b0788c757
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-17 16:20:42 +0700

    membuat petugas1

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -151,10 +151,10 @@ int main() {
     Buku* buku1 = new Buku();
     buku1->setJudul("Ilmu Sosial").setPenulis("Dani Ramadhan");
 
-    
+    Peminjam* siswa1 = new Peminjam();
+    siswa1->setNama("Rendra").setId(220);
 
-    Petugas* petugas1 = new Petugas();
-    petugas1->setNama("Nico").setId(10).setLevelAkses("Biasa");
+    
 
     Admin admin;
 
